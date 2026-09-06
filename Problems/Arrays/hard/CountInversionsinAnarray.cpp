e83650c4142b2fba6940559f3b9b9791b09f6181
bhavesh joshi <joshbhavesh10@gmail.com>
2026-09-06 19:54:39 +0530

    today's practice

---
+++ Problems/Arrays/hard/CountInversionsinAnarray.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int Sort(vector<int>&vec , int high , int low)
+{
+    int count = 0;
+    int mid = (high + low)/2;
+
+    if(low>=high)
+    {
+        return;
+    }
+    count = count + Sort(vec , mid , low);
+    count = count + Sort(vec , mid+1 , high);
+    count = count + merge(vec , high , mid , low);
+    return count;
+}
+int merge(vector<int>&vec , int high , int mid , int low)
+{
+    int count = 0;
+    int left = low;
+    int right = mid+1;
+    vector<int>temp;
+    while(left<=mid && right<=high)
+    {
+        if(vec[left]>=vec[right])
+        {
+            count = count + ((mid-left)+1);
+            temp.push_back(vec[right]);
+            right++;
+        }
+        else
+        {
+            temp.push_back(vec[left]);
+            left++;
+        }
+    }
+    while(left<=mid)
+    {
+        temp.push_back(vec[left]);
+        left++;
+    }
+    while(left<=mid)
+    {
+        temp.push_back(vec[left]);
+        left++;
+    }
+    for(int i = low ; i<=high ; i++)
+    {
+        vec[i] = temp[i-low];
+    }
+    return count;
+}
+int main()
+{
+    vector<int>vec;
+    int n;
+    cin>>n;
+
+    for(int i = 0 ; i<n ; i++)
+    {
+        int no;
+        cin>>no;
+        vec.push_back(no);
+    }
+
+    // Brute force;
+    // int cnt = 0;
+    // for(int i = 0 ; i<vec.size() ; i++)
+    // {
+    //     for(int j = i+1 ; j<vec.size() ; j++)
+    //     {
+    //         if(vec[i]>=vec[j])
+    //         {  
+    //             cnt++ ;
+    //         }
+    //     }
+    // }
+    // cout<<cnt<<endl;
+
+    // Using merge Sort
+    cout<<Sort(vec , vec.size()-1 , 0)<<endl;
+    return 0;
+}
--- Problems/Arrays/hard/FindtherepeatingandmissingNumber.cpp
+++ Problems/Arrays/hard/FindtherepeatingandmissingNumber.cpp
@@ -46,20 +46,64 @@ int main()
     // }
 
     // Better Approach
-    unordered_map<int,int>mp;
-    vector<int>ans;
-    int sum = (vec.size()*(vec.size()+1))/2;
-    int rep = -1;
-    for(int i = 0 ; i<vec.size() ; i++)
+    // Using Maps
+    // unordered_map<int,int>mp;
+    // vector<int>ans;
+    // int sum = (vec.size()*(vec.size()+1))/2;
+    // int rep = -1;
+    // for(int i = 0 ; i<vec.size() ; i++)
+    // {
+    //     sum = sum-vec[i]; 
+    //     mp[vec[i]]++;
+    //     if(mp[vec[i]]>1)
+    //     {
+    //         rep = vec[i];
+    //     }
+    // }
+    // ans.push_back(rep);
+    // ans.push_back(sum+rep);
+
+    // using hashing 
+    // vector<int>arr(vec.size() , 0);
+    // vector<int>ans;
+
+    // for(int i = 0 ; i<vec.size() ; i++)
+    // {
+    //     arr[vec[i]]++;
+    // }
+    // int rep = -1 , miss = -1;
+    // for(int j = 0 ; j<arr.size() ; j++)
+    // {
+    //     if(arr[j] > 1)
+    //     {
+    //         rep = j;
+    //     }
+    //     if(arr[j] == 0)
+    //     {
+    //         miss = j;
+    //     }
+    // }
+    // ans.push_back(rep);
+    // ans.push_back(miss);
+
+    // Best Approach 
+    // using equations
+    long long int n = vec.size();
+    long long int Ns = (n*(n+1))/2;
+    long long int N2s = (n*(n+1)*((2*n)+1))/6;
+    long long int aNs = 0 , aN2s = 0 ; 
+
+    for(long long int i = 0 ; i<vec.size() ; i++)
     {
-        sum = sum-vec[i]; 
-        mp[vec[i]]++;
-        if(mp[vec[i]]>1)
-        {
-            rep = vec[i];
-        }
+        aNs += vec[i];
+        aN2s += (vec[i]*vec[i]);
     }
-    ans.push_back(rep);
-    ans.push_back(sum+rep);
-    return 0;
-}
+
+    long long int diff1 = aNs - Ns , diff2 = aN2s - N2s;
+    long long int dev = diff2/diff1;
+    long long int rep = (dev + diff1)/2;
+    
+    cout<<"ans is "<<rep<<" "<<rep-diff1<<endl;
+
+    return 0; 
+ }
